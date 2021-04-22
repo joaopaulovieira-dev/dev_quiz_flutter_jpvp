aba27fbd60c3350276111cd04fa99155f8494809
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-04-22 00:32:19 -0300

    Implementação da AppBar e Card Score

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,4 +1,6 @@
 import 'package:dev_quiz_flutter_jpvp/core/app_gradients.dart';
+import 'package:dev_quiz_flutter_jpvp/core/app_text_styles.dart';
+import 'package:dev_quiz_flutter_jpvp/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -7,20 +9,43 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            child: Row(
+            child: Stack(
               children: [
-                Text("Olá, João Paulo :D"),
                 Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              'https://avatars.githubusercontent.com/u/80857646?v=4'))),
-                )
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: 'joaopaulovieira.dev',
+                              style: AppTextStyles.titleBold,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/80857646?v=4'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
               ],
             ),
           ),
