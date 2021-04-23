3aadcbc4b9a96de96731494430c7687fb6d26262
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-04-23 00:17:02 -0300

    Adicionado LevelButton

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:dev_quiz_flutter_jpvp/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz_flutter_jpvp/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 19),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: 'Fácil',
+            ),
+            LevelButtonWidget(
+              label: 'Médio',
+            ),
+            LevelButtonWidget(
+              label: 'Difícil',
+            ),
+            LevelButtonWidget(
+              label: 'Perito',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
